94eb84c5f3c20f5daec24f927a017cb4124cb659
Ricardo Cruz <ricardoprodev2025@gmail.com>
2026-03-06 11:46:41 +0000

    PT CV

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -186,14 +186,24 @@ function Resume() {
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-      <Button
-        href="/Ricardo_Cruz_CV.html"
-        variant="secondary"
-        className="group mt-6 w-full"
-      >
-        Download CV
-        <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
-      </Button>
+      <div className="mt-6 flex gap-3">
+        <Button
+          href="/Ricardo_Cruz_CV.html"
+          variant="secondary"
+          className="group w-full"
+        >
+          CV (EN)
+          <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
+        </Button>
+        <Button
+          href="/Ricardo_Cruz_CV_PT.html"
+          variant="secondary"
+          className="group w-full"
+        >
+          CV (PT)
+          <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
+        </Button>
+      </div>
     </div>
   )
 }
